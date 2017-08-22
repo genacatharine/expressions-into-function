8eaf5770341eb70a74e29e1a86f7e78d2b0957f6
Gena Israel <gena.catharine@gmail.com>
2017-08-22 08:14:20 -0600

    functions

--- abstract-functions.js
+++ abstract-functions.js
@@ -1,34 +1,25 @@
 // Clean up the code below with a single function called `personalIntroduction`
 // The new code should have the exact same functionality and output.
+function personalIntroduction(name, height, favoriteColor) {
 
-var name = "Bob";
-var height = "72";
-var favoriteColor = "red";
 var string = "My name is " + name;
 string += ", and my height is " + height;
 string += ", and my favorite color is " + favoriteColor;
 console.log(string);
-
-var name = "Samantha";
-var height = "65";
-var favoriteColor = "orange";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
-
-var name = "Christopher";
-var height = "69";
-var favoriteColor = "green";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
-
-var name = "Tamera";
-var height = "63";
-var favoriteColor = "blue";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
+}
+personalIntroduction("mary", 42, "red")
+// var name = "Bob";
+// var height = "72";
+// var favoriteColor = "red";
+//
+// var name = "Samantha";
+// var height = "65";
+// var favoriteColor = "orange";
+//
+// var name = "Christopher";
+// var height = "69";
+// var favoriteColor = "green";
+//
+// var name = "Tamera";
+// var height = "63";
+// var favoriteColor = "blue";
